7672999f106eceb8119228878dd48ce805820963
Rimonkumar <22-46957-1@student.aiub.edu>
2025-07-12 15:49:33 +0600

    UsageOfDTO

---
+++ UsageOfDTO/UsageOfDTO/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UsageOfDTO.DTOs;
+using UsageOfDTO.EF;
+
+namespace UsageOfDTO.Controllers
+{
+    public class StudentController : Controller
+    {
+        private Sp25_CEntities db = new Sp25_CEntities();
+        // GET: Student
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(Convert(data));
+        }
+        [HttpGet]
+        public ActionResult Create() { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(StudentDTO s) {
+            if (ModelState.IsValid) {
+                db.Students.Add(Convert(s));
+                db.SaveChanges();
+            }
+            return View(s);
+        }
+
+        public static Student Convert(StudentDTO s) {
+            return new Student() { 
+                Name = s.FName + " "+s.LName,
+                Cgpa = s.Cgpa,
+                Dob = s.Dob,
+                Age = DateTime.Now.Year - s.Dob.Year
+            };
+        }
+        public static StudentDTO Convert(Student s)
+        {
+            var name = s.Name.Split(' ');
+            return new StudentDTO()
+            {
+                FName = name[0],
+                LName= name[1],
+                Cgpa = s.Cgpa,
+                Dob = s.Dob,
+                
+            };
+        }
+        public static List<StudentDTO> Convert(List<Student> data) { 
+            var list = new List<StudentDTO>();
+            foreach (var item in data) { 
+                list.Add(Convert(item));
+            }
+            return list;
+        }
+    }
+}
---
+++ UsageOfDTO/UsageOfDTO/DTOs/StudentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UsageOfDTO.DTOs
+{
+    public class StudentDTO
+    {
+        [Required]
+        [StringLength(50)]
+        public string FName { get; set; }
+        [Required]
+        [StringLength (50)]
+        public string LName { get; set; }
+        public DateTime Dob { get; set; }
+        public double Cgpa { get; set; }
+    }
+}
