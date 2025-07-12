fd742db495b23fa6a46d1cbcba8e5a8e7a634f9e
Rimonkumar <22-46957-1@student.aiub.edu>
2025-07-12 15:33:24 +0600

    From submistion

---
+++ Form Submission/Form Submission/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Form_Submission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_Submission.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult Index() {
+            return View(new Student());
+        }
+        [HttpPost]
+        public ActionResult Index(Student s) {
+            if (ModelState.IsValid) { //checking the model validation
+                return RedirectToAction("Index","Home");
+            }
+            return View(s);
+        }
+        //[HttpPost]
+        //public ActionResult Index(string Name, string Id, string Email)
+        //{
+        //    ViewBag.Name = Name;
+        //    ViewBag.Email = Email;
+
+        //    ViewBag.Id = Id;
+        //    //ViewBag.Gn = ;
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Index(FormCollection fc)
+        //{
+        //    ViewBag.Name = Request["Name"];
+        //    ViewBag.Email = Request["Email"];
+
+        //    ViewBag.Id = fc["Id"];
+        //    ViewBag.Gn = fc["Gender"];
+        //    return View();
+        //}
+    }
+}
---
+++ Form Submission/Form Submission/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Form_Submission.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please provide  Id")]
+        [Range(1,40)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(10,ErrorMessage ="Name should not exceed 10 chars")]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(30)]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        public DateTime Dob { get; set; }
+        public string[] Hobbies { get; set; }
+    }
+}
